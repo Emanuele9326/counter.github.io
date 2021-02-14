e5a9218a94112097fd73b0510b677ff5b06573eb
EMANUELE <65012625+Emanuele9326@users.noreply.github.com>
2021-02-14 09:31:09 +0100

    Progetto:relizzazione di un counter

---
+++ js/counter.js
@@ -0,0 +1,33 @@
+function incremento(event){
+
+let valore=document.getElementById("numero").getAttribute("value");
+
+let numero;
+numero=parseInt(valore);
+numero++;
+
+document.getElementById("numero").setAttribute("value",numero);
+}
+
+
+function decremento(event){
+
+let valore=document.getElementById("numero").getAttribute("value");
+let numero;
+numero=parseInt(valore);
+
+if(valore>0){      //esegue il controllo del valore del counter.Sè il valore=0 il decremento non viene eseguito
+ numero--;
+};
+
+document.getElementById("numero").setAttribute("value",numero);
+}
+
+function reset(event){
+document.getElementById("numero").setAttribute("value",0);
+}
+
+
+document.getElementById("incremento").addEventListener("click",incremento);
+document.getElementById("decremento").addEventListener("click",decremento);
+document.getElementById("Reset").addEventListener("click",reset);
