d92e177e7d21c06d979107ddedbadd9de81b8010
EMANUELE <emanuelemarra93@outlook.com>
2021-02-15 11:48:49 +0100

    Progetto: realizzazione di un counter

--- js/counter.js
+++ js/counter.js
@@ -19,14 +19,12 @@ numero=parseInt(valore);
 if(valore>0){      //esegue il controllo del valore del counter.Sè il valore=0 il decremento non viene eseguito
  numero--;
 };
-
 document.getElementById("numero").setAttribute("value",numero);
 }
 
 function reset(event){
 document.getElementById("numero").setAttribute("value",0);
 }
-
 
 document.getElementById("incremento").addEventListener("click",incremento);
 document.getElementById("decremento").addEventListener("click",decremento);
